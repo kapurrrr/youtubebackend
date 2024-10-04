ca4bbb6a2c519214f358b4cb1f33f7750db50795
Lakshay Kapur <kapur.lakshay8@gmail.com>
2024-10-05 01:16:17 +0530

    utils code

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+const asyncHandler = () => {
+    return (res, req, next) => {
+        Promise.resolve(requesHandler(req,res,next)).catch((err)=>next(err))
+    }
+}
+
+
+
+
+export {asyncHandler}
+
+// const asyncHandler = (fn) => (req,res,next) => {
+//     try{
+//         await fn(req,res,next)
+//     }catch(error){
+//         res.send(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
